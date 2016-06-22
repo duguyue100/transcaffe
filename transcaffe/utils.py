5653232bab68076674ab473efe7123095c5d038d
Yuhuang Hu <duguyue100@gmail.com>
2016-06-22 17:21:25 +0200

    update for directory checker

--- transcaffe/utils.py
+++ transcaffe/utils.py
@@ -23,3 +23,28 @@ def file_checker(filename):
         raise ValueError("File %s is not existed!" % (filename))
 
     return True
+
+
+def dir_checker(dir_name, make_dir=False):
+    """Check if the directory is existed.
+
+    Parameters
+    ----------
+    dir_name : string
+        an absolute path to a directory
+    make_dir : bool
+        if create it when the directory is not existed.
+
+    Returns
+    -------
+    abort if the directory is not existed
+    """
+    if not os.path.isdir(dir_name):
+        if make_dir is True:
+            # make sure you have right permession, otherwise will raise
+            # OSError
+            os.mkdirs(dir_name)
+        else:
+            raise ValueError("Directory %s is not existed!" % (dir_name))
+
+    return True
