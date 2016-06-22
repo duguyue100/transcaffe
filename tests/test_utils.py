b2aabb89a2b22f33f173d58bce1e9a3893d3d132
Yuhuang Hu <duguyue100@gmail.com>
2016-06-22 17:22:31 +0200

    update testing function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,3 +14,8 @@ from nose.tools import assert_raises
 def test_file_checker():
     """test file_checker function."""
     assert_raises(ValueError, utils.file_checker, "/a/b/c")
+
+
+def test_dir_checker():
+    """test directory checker function."""
+    assert_raises(ValueError, utils.dir_checker, "/a/b/c", False)
