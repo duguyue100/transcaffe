27b056e876eee0c5c5927b6b05c74f5a7492a556
Yuhuang Hu <duguyue100@gmail.com>
2016-07-01 16:50:56 +0200

    update mean file parsing function

--- transcaffe/parsing.py
+++ transcaffe/parsing.py
@@ -6,7 +6,51 @@ Author: Yuhuang Hu
 Email : duguyue100@gmail.com
 """
 
+from __future__ import print_function
+
+import numpy as np
+from scipy.io import loadmat
 from transcaffe import caffe_pb2, utils
+from google.protobuf.text_format import Merge
+
+
+def parse_mean_file(filename, mode="proto"):
+    """Parse a mean file by given path
+
+    TODO: complete more options based on different Caffe Models
+
+    Parameters
+    ----------
+    filename : string
+        absolute path of the mean file
+    mode : string
+        "proto" for .binaryproto file
+        "mat" for MAT binary file
+
+    Returns
+    -------
+    mean_mat : numpy.ndarray
+        an array that contains the mean values
+    """
+    utils.file_checker(filename)
+
+    if mode == "proto":
+        tp = caffe_pb2.TransformationParameter()
+        f = open(filename, mode="rb")
+        mean_contents = f.read()
+        f.close()
+        tp.ParseFromString(mean_contents)
+        mean_mat = np.array(tp.mean_value).reshape((3,
+                                                    tp.crop_size,
+                                                    tp.crop_size))
+        mean_mat = np.transpoe(mean_mat, (1, 2, 0))
+    elif mode == "mat":
+        # based on VGG's Mat file.
+        mean_contents = loadmat(filename)
+        mean_mat = mean_contents["image_mean"]
+        print(mean_mat.shape)
+
+    return mean_mat
 
 
 def parse_protobuf(filename):
@@ -18,3 +62,39 @@ def parse_protobuf(filename):
         absolute path of .prototxt file
     """
     utils.file_checker(filename)
+
+    f = open(filename, mode="rb")
+
+    net_param = caffe_pb2.NetParameter()
+
+    Merge(f.read(), net_param)
+
+    print(net_param.name)
+
+    print(type(net_param))
+
+    print(dir(net_param))
+
+    f.close()
+
+
+def get_input_size(net_param):
+    """Get input parameters, or guess one at least.
+
+    Parameters
+    ----------
+    net_param : caffe_pb2.NetParameter
+        structure that contains all the network parameters
+
+    Returns
+    -------
+    in_size : tuple
+        tuple that defines the input size
+    """
+    if len(net_param.input_dim) != 0:
+        return net_param.input_dim
+    elif len(net_param.input_shape) != 0:
+        return net_param.input_shape
+    else:
+        print("[MESSAGE] Couldn't find Input shape in the Network Parameters."
+              "The returned shape is inferenced from the network name")
