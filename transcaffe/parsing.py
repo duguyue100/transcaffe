c126f09f12c98e0ecbe7a65e3ca027c9dbe86364
Yuhuang Hu <duguyue100@gmail.com>
2016-07-29 16:14:43 +0200

    update parsing functions

--- transcaffe/parsing.py
+++ transcaffe/parsing.py
@@ -1,6 +1,6 @@
 """Parse CaffeModel.
 
-Helped by caffe2theano
+Helped by caffe2theano, MarcBS's Caffe2Keras module.
 
 Author: Yuhuang Hu
 Email : duguyue100@gmail.com
@@ -13,6 +13,18 @@ from scipy.io import loadmat
 from transcaffe import caffe_pb2, utils
 from google.protobuf.text_format import Merge
 
+v1_map = {0: 'NONE', 1: 'ACCURACY', 2: 'BNLL', 3: 'CONCAT', 4: 'CONVOLUTION',
+          5: 'DATA', 6: 'DROPOUT', 7: 'EUCLIDEANLOSS', 8: 'FLATTEN',
+          9: 'HDF5DATA', 10: 'HDF5OUTPUT', 11: 'IM2COL', 12: 'IMAGEDATA',
+          13: 'INFOGAINLOSS', 14: 'INNERPRODUCT', 15: 'LRN',
+          16: 'MULTINOMIALLOGISTICLOSS', 17: 'POOLING', 18: 'RELU',
+          19: 'SIGMOID', 20: 'SOFTMAX', 21: 'SOFTMAXWITHLOSS', 22: 'SPLIT',
+          23: 'TANH', 24: 'WINDOWDATA', 25: 'ELTWISE', 26: 'POWER',
+          27: 'SIGMOIDCROSSENTROPYLOSS', 28: 'HINGELOSS', 29: 'MEMORYDATA',
+          30: 'ARGMAX', 31: 'THRESHOLD', 32: 'DUMMY_DATA', 33: 'SLICE',
+          34: 'MVN', 35: 'ABSVAL', 36: 'SILENCE', 37: 'CONTRASTIVELOSS',
+          38: 'EXP', 39: 'DECONVOLUTION'}
+
 
 def parse_caffemodel(filename):
     """Parse a given caffemodel.
@@ -24,6 +36,10 @@ def parse_caffemodel(filename):
 
     Returns
     -------
+    layers : list
+        The list representation of the network
+    version : string
+        pretrined network version
     """
     utils.file_checker(filename)
 
@@ -34,11 +50,8 @@ def parse_caffemodel(filename):
     f.close()
 
     net_param.ParseFromString(contents)
-    layers = get_layers(net_param)
 
-    layers_dict = {}
-    for layer in layers:
-        layers_dict[layer.name] = layer.blobs
+    return get_layers(net_param)
 
 
 def parse_mean_file(filename, mode="proto"):
@@ -87,22 +100,21 @@ def parse_protobuf(filename):
     ----------
     filename : string
         absolute path of .prototxt file
+
+    Returns
+    -------
+    net_param : caffe_pb2.NetParameter
+        The parsed .prototxt structure.
     """
     utils.file_checker(filename)
 
     f = open(filename, mode="rb")
-
     net_param = caffe_pb2.NetParameter()
-
+    # Check before Merge? For V1?
     Merge(f.read(), net_param)
-
-    print(net_param.name)
-
-    print(type(net_param))
-
-    print(dir(net_param))
-
     f.close()
+
+    return net_param
 
 
 def get_layers(net_param):
@@ -111,16 +123,39 @@ def get_layers(net_param):
     Parameters
     ----------
     net_param : caffe_pb2.NetParameter
+        A pretrined network description.
+    Returns
+    -------
+    layers : list
+        description of the layers.
+    version : string
+        version information of the pretrained model.
+    """
+    if len(net_param.layers) > 0:
+        return net_param.layers[:], "V1"
+    elif len(net_param.layer) > 0:
+        return net_param.layer[:], "V2"
+    else:
+        raise Exception("Couldn't find layers!")
+
+
+def get_layer_type(layer):
+    """Get a given layer type.
+
+    Parameters
+    ----------
+    layer : caffe_pb2.V1LayerParameter
+        a given layer in the network
 
     Returns
     -------
+    type : int or string
+        type of the layer.
     """
-    if len(net_param.layers) > 0:
-        return net_param.layers
-    elif len(net_param.layer) > 0:
-        return net_param.layer
+    if type(layer.type) == int:
+        return str(v1_map[layer.type]).lower()
     else:
-        raise Exception("Couldn't find layers!")
+        return str(layer.type).lower()
 
 
 def get_input_size(net_param):
@@ -143,6 +178,7 @@ def get_input_size(net_param):
     else:
         print("[MESSAGE] Couldn't find Input shape in the Network Parameters."
               "The returned shape is inferenced from the network name")
+        return []
 
 
 def get_weights_raw(blobs):
